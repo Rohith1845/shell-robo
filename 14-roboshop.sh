d06229028272f9b59761733c6a95d769afaa67f6
rohith <krishnamsettyrohith@gmail.com>
2025-09-28 02:55:14 +0530

    success

--- 14-roboshop.sh
+++ 14-roboshop.sh
@@ -5,12 +5,12 @@ SG_ID= sg-045b3478cf0d8062e""  #Replace with your security group id
 
 for instance in $@
 do
-    INSTANCE_ID=a$(ws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-045b3478cf0d8062e --tag-specifications "ResourceType=instance,xTags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstancesId' --output text)
+    INSTANCE_ID=a$(ws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,xTags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstancesId' --output text)
 
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids i-05493202d91f9ab56 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids i-05493202d91f9ab56 --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
     echo "$instance: $"
 
