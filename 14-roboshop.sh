60662a85ceea12106182b477de2e5ed674f92e5b
rohith <krishnamsettyrohith@gmail.com>
2025-09-28 02:51:55 +0530

    success

---
+++ 14-roboshop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID= sg-045b3478cf0d8062e""  #Replace with your security group id
+
+for instance in $@
+do
+    INSTANCE_ID=a$(ws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-045b3478cf0d8062e --tag-specifications "ResourceType=instance,xTags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstancesId' --output text)
+
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids i-05493202d91f9ab56 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids i-05493202d91f9ab56 --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+    echo "$instance: $"
+
+done 
